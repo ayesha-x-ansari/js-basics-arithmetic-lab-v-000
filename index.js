3c09ee939d71d400c6b26ab6929ab7c6ad37e165
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-08-02 01:03:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ You should NOT try to redefine any of the above variables.
 
   const randomNumber =  Math.floor(Math.random() * 20) + 1;
 
-  
+  const randomNumber =  Math.floor(Math.random() * 20) + 1;
